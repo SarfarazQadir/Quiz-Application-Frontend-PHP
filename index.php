76e1a8e65ce94a97b6b0a2d24fa3dc1220e374cb
SarfarazQadir <sarfarazqadir594@gmail.com>
2024-04-14 13:25:29 +0500

    2ND COMMIT

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include("connection.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Quiz Application</title>
+    <title>Simple Quiz Application</title>
     <!-- Include Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <style>
